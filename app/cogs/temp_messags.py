2d475965b13d7799c263fbd1f2c05569079f9e45
yoyomaster9 <yoyomaster9@yahoo.com>
2025-02-12 18:42:16 -0800

    drop /setdelay

--- app/cogs/temp_messags.py
+++ app/cogs/temp_messags.py
@@ -28,36 +28,7 @@ class TempMessageCog(commands.Cog):
             #     file.write(s)
             await asyncio.sleep(config['TempMessageTime'])
             await message.delete()
-    
-    @discord.app_commands.command(
-        name = 'setdelay',
-        description = 'Sets the time delay for temp message' 
-    )
-    async def setdelay(self, interaction: discord.Interaction, delay: str) -> None:
 
-        pattern = r'(?:(\d+)\s*(?:hours|hour|hrs|hr|h))?\s*(?:(\d+)\s*(?:minutes|minute|mins|min|m))?\s*(?:(\d+)\s*(?:seconds|second|secs|sec|s))?'
-        matches = re.findall(pattern, delay)
-        l = [''.join(m[i] for m in matches) for i in range(3)]
-        l = [int(x) if x != '' else 0 for x in l]
-
-        sec = l[0] * 3600 + l[1] * 60 + l[2]
-        config['TempMessageTime'] = sec
-
-        
-        hours, minutes = divmod(sec, 3600)
-        minutes, seconds = divmod(minutes, 60)
-        output = ''
-        if hours > 0:
-            output += f'{hours}hr '
-        if minutes > 0:
-            output += f'{minutes}m '
-        if seconds > 0:
-            output += f'{seconds}s'
-
-        
-        await interaction.response.send_message(f'Temp message chat delay set to {output}')
-        temp_channel = self.bot.get_channel(config['TempMessageChannelID'])
-        await temp_channel.edit(topic = f"Messages will be deleted after {output} ")  
 
 async def setup(bot):
 
